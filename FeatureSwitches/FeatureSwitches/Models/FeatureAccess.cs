ed2d4c1fb4f14c3dd93c306300d3daf0d8ac8ac9
Mohit <mohit@iris.com.my>
2021-04-26 11:31:02 +0800

    First Commit

---
+++ FeatureSwitches/FeatureSwitches/Models/FeatureAccess.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace FeatureSwitches.Models
+{
+    public partial class FeatureAccess
+    {
+        public decimal FeatureId { get; set; }
+        public string FeatureName { get; set; }
+        public string UserEmail { get; set; }
+        public bool? IsAccessible { get; set; }
+    }
+}
---
+++ FeatureSwitches/FeatureSwitches/Models/FeatureDBContext.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace FeatureSwitches.Models
+{
+    public partial class FeatureDBContext : DbContext
+    {
+        public FeatureDBContext()
+        {
+        }
+
+        public FeatureDBContext(DbContextOptions<FeatureDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<FeatureAccess> FeatureAccesses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS;Database=FeatureDB;Integrated Security=True");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<FeatureAccess>(entity =>
+            {
+                entity.HasKey(e => e.FeatureId);
+
+                entity.ToTable("FeatureAccess");
+
+                entity.Property(e => e.FeatureId)
+                    .HasColumnType("numeric(18, 0)")
+                    .HasColumnName("FeatureID");
+
+                entity.Property(e => e.FeatureName).HasMaxLength(150);
+
+                entity.Property(e => e.IsAccessible).HasColumnName("isAccessible");
+
+                entity.Property(e => e.UserEmail).HasMaxLength(250);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
